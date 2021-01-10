024656ec314008faff8c3760d679a3d0287d5d81
Jonathan Zeller <jonathan_zeller@outlook.com>
2021-01-10 23:14:32 +0100

    Renamed entire UI to german, there might be some errors.

--- calculator_interface.py
+++ calculator_interface.py
@@ -13,93 +13,93 @@ print("========   \\\      //       //\\\   ==========   ||    ||")
 print("      ||   ||\\\  //||      //  \\\      ||       ||    ||")
 print("      ||   || \\\// ||    =======\\\     ||       ======||")
 print("      //   ||      ||    //      \\\    ||       ||    ||")
-print("\\\===//    ||      ||   //        \\\   ||       ||    ||")
-print("     -      S h a p e   C a l c u l a t o r      -\n\n")
+print("\\\===//    ||      ||   //        \\\   ||       ||    ||\n\n")
 
 saved_letter = check_input.letter(
-    "What shape are you working with?\nTrapeze <T> | Parallelogram <P> | Triangle <I> | Rectangle <R>", "TPIR", True)
+    "Mit welcher Form arbeitest du?\nTrapez <T> | Parallelogramm <P> | Dreieck <D> | Rechteck <R>", "TPDR", True)
 
 if saved_letter == "T":
     saved_letter = check_input.letter(
-        "What do you want to calculate about the Trapeze?\nSurface Area <S> | Middle line <M> | Height <H>", "SMH",
+        "Was willst du vom Trapez berechenen??\nFlächeninhalt <F> | Länge der Mittellinie <M> | Höhe <H>", "FMH",
         True)
 
-    if saved_letter == "S":
-        print("The trapeze has a surface area of: " + str(
-            JMath.trapeze.surface(check_input.number("Please enter the length of the middle line."),
+    if saved_letter == "F":
+        print("Das Trapez hat eine Oberfläche von: " + str(
+            JMath.trapeze.surface(check_input.number("Bitte die Höhe eingeben."),
                                   JMath.trapeze.middle_line(
-                                      check_input.number("Please enter the length of the upper line."),
-                                      check_input.number("Please enter the length of the lower line.")))))
+                                      check_input.number("Bitte die Länge der oberen Linie eingeben."),
+                                      check_input.number("Bitte die Länge der unteren Linie eingeben.")))))
 
     elif saved_letter == "M":
         saved_letter = check_input.letter(
-            "From what values do you want to calculate the middle line?\nUpper and lower lines <U> | Surface area and height <S>",
-            "US", True)
+            "Von welchen Werten willst du die Länge der Mittellinie berechnen?\nObere und untere Linie <L> | Oberfläche und Höhe <O>",
+            "OL", True)
 
-        if saved_letter == "U":
-            print("The length of the middle line is: " + str(
-                JMath.trapeze.middle_line(check_input.number("Please enter the length of the first line."),
-                                          check_input.number("Please enter the length of the second line."))))
+        if saved_letter == "L":
+            print("Die Länge der Mittelline ist: " + str(
+                JMath.trapeze.middle_line(check_input.number("Bitte die Länge der oberen Linie eingeben."),
+                                          check_input.number("Bitte die Länge der unteren Linie eingeben."))))
 
-        elif saved_letter == "S":
-            print("The length of the middle line is: " + str(JMath.trapez.middle_line(check_input.number(
-                "Please enter the length of the upper line."),
-                check_input.number("Please enter the length of the lower line."))))
+        elif saved_letter == "O":
+            print("Die Länge der Mittelline ist: " + str(JMath.trapez.height_or_middle_line(check_input.number(
+                "Bitte die Höhe eingeben."),
+                check_input.number("Bitte den Flächeninhalt eingeben."))))
 
     elif saved_letter == "H":
-        print("The height is: " + str(JMath.trapeze.height_or_middle_line(check_input.number(
-            "Please enter the length of the upper line."),
-            check_input.number("Please enter the length of the lower line."))))
+        print("Die Höhe ist:" + str(JMath.trapeze.height_or_middle_line(check_input.number(
+            "Bitte die Länge der Mittellinie eingeben."),
+            check_input.number("Bitte den Flächeninhalt eingeben."))))
 
 
 elif saved_letter == "P":
     saved_letter = check_input.letter(
-        "What do you want to calculate about the parallelogram?\nSurface area <S> | Height <H> | Base line <B>", "SHB",
+        "Was willst du vom Parallelogramm berechnen?\nFlächeninhalt <F> | Höhe <H> | Länge der Grundlinie <G>", "FHG",
         True)
 
-    if saved_letter == "S":
-        print("The surface area is: " + str(
-            JMath.parallelogram.surface(check_input.number("Please enter the length of the base line."),
-                                        check_input.number("Please enter height."))))
+    if saved_letter == "F":
+        print("Der Flächeninhalt ist: " + str(
+            JMath.parallelogram.surface(check_input.number("Bitte die Länge der Grundlinie eingeben."),
+                                        check_input.number("Bitte die Höhe eingeben."))))
     if saved_letter == "H":
-        print("The height is: " + str(
-            JMath.parallelogram.height_or_base_line(check_input.number("Please enter the length of the base line."),
-                                                    check_input.number("Please enter the surface area."))))
+        print("Die Höhe ist: " + str(
+            JMath.parallelogram.height_or_base_line(check_input.number("Bitte die Länge der Grundlinie eingeben."),
+                                                    check_input.number("Bitte den Flächeninhalt eingeben."))))
 
-    if saved_letter == "B":
-        print("The length of the base line is: " + str(
-            JMath.parallelogram.height_or_base_line(check_input.number("Please enter the height"),
-                                                    check_input.number("Please enter the surface area."))))
+    if saved_letter == "G":
+        print("Die Länge der Grundlinie ist:  " + str(
+            JMath.parallelogram.height_or_base_line(check_input.number("Bitte die Höhe eingeben."),
+                                                    check_input.number("Bitte den Flächeninhalt eingeben."))))
 
 elif saved_letter == "I":
     saved_letter = check_input.letter(
-        "What do you want to calculate about the triangle?\nSurface area <S> | Height <H> | Base line length <B>",
-        "SHB", True)
+        "Was willst du vom Dreieck berechnen?\nOberfläche <O> | Höhe <H> | Länge der Grundlinie <G>",
+        "OHG", True)
 
-    if saved_letter == "S":
-        print("The triangle has a surface of: " + str(JMath.triangle.surface(check_input.number(
-            "Please enter the length of the base line."), check_input.number("Please enter the height."))))
+    if saved_letter == "O":
+        print("Das Dreieck hat eine Oberfläche von: " + str(JMath.triangle.surface(check_input.number(
+            "Bitte die Länge der Grundlinie eingeben."), check_input.number("Bitte die Höhe eingeben."))))
 
     elif saved_letter == "H":
-        print("The triangle has a height of: " + str(JMath.triangle.height_or_base_line(check_input.number(
-            "Please enter the length of the base line."), check_input.number("Please enter the surface area."))))
+        print("Das Dreieck hat eine Höhe von: " + str(JMath.triangle.height_or_base_line(check_input.number(
+            "Bitte die Länge der Grundlinie eingeben."), check_input.number("Bitte die Oberfläche eingeben."))))
 
-    elif saved_letter == "B":
-        print("The triangle has a base line length of: " + str(JMath.triangle.height_or_base_line(check_input.number(
-            "Please enter the length of the height."), check_input.number("Please enter the surface area."))))
+    elif saved_letter == "G":
+        print("Die Länge der Grundlinie ist: " + str(JMath.triangle.height_or_base_line(check_input.number(
+            "Bitte die Höhe eingeben."), check_input.number("Bitte die Oberfläche eingeben."))))
 
 elif saved_letter == "R":
     saved_letter = check_input.letter(
-        "What do you want to calculate about the rectangle?\nSurface <S> | Side length <L>", "SL", True)
+        "Was willst du vom Rechteck berechnen\nOberfläche <O> | Seitenlänge <L>", "OL", True)
 
-    if saved_letter == "S":
-        print("The rectangle has a surface area of: " + str(
-            JMath.rectangle.surface(check_input.number("Please enter the length of the top or bottom lines."),
-                                    check_input.number("Please enter the length of the right or left lines."))))
+    if saved_letter == "O":
+        print("Das Rechteck hat eine Oberfläche von: " + str(
+            JMath.rectangle.surface(check_input.number("Bitte die Länge der Seiten a oder c eingeben."),
+                                    check_input.number("Bitte die Länge der Seiten b oder d eingeben."))))
 
     elif saved_letter == "L":
-        print("The line has a length of: " + JMath.rectangle.side(
-            check_input.number("Please enter the surface area of the rectangle."),
-            check_input.number("Please enter the length of the side turned 90° to the current side.")))
+        print("Die Seitenlänge ist: " + JMath.rectangle.side(
+            check_input.number("Bitte die Oberfläche eingeben."),
+            check_input.number(
+                "Bitte die Längen der Seiten a oder c eingeben, wenn mit b oder d gerechnet wird, oder die Länge der Seiten b oder d, wenn mit a oder c gerechnet wird.")))
 
 input()
