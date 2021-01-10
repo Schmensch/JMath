70b675dc9a771ed928c6a1fedf04be854b2a7aa2
Jonathan Zeller <jonathan_zeller@outlook.com>
2021-01-10 23:51:44 +0100

    calculator_interface.py is now a class. main.py is the new program to launch.
    Added help startup option.

---
+++ main.py
@@ -0,0 +1,16 @@
+# ---------------
+# Imports
+# ---------------
+import sys
+
+from calculator_interface import calculator_interface
+
+try:
+    if sys.argv[1] == "-h" or "--hilfe" or "--help" or "/?" or "/h" or "/help" or "/hilfe":
+        print("JMath hilfe:")
+        print(
+            "JMath ist eine Konsolenanwendung, die man benutzt um unterschiedliche Werte von Dreiecken, Rechtecken, Parallelogrammen oder Trapezen zu berechnen.")
+        print("Um JMath zu starten, klicke doppelt auf die Datei.")
+
+except range:
+    calculator_interface
